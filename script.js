4adef18aecb72939dca6787cb5a4d1362cbd2d12
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-14 19:51:55 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("title").style.display = 'block';
+function gameStart(){
+  document.getElementById("title").style.display = 'none';
+}
+console.log(document.getElementById("gameSpace"));
