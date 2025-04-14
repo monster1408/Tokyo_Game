8e74f540607bef1060420da4c24d980acb75831e
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-14 19:47:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.getElementById("title").style.display = 'block';
 function gameStart(){
   document.getElementById("title").style.display = 'none';
 }
+console.log(document.getElementById("gameSpace"));
