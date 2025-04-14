4291953c9b2759139bf120426c114f25124d9f19
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-14 20:38:18 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-gamespace = document.getElementById("gameSpace")
-console.log(document.getElementById("gameSpace"));
-document.getElementById("title").style.display = 'block';
-function gameStart(){
-  document.getElementById("title").style.display = 'none';
-  gamespace.innerHTML = "<p message>a</p>";
-
-}
