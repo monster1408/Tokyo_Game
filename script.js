7902174100d37cab5d1ec37bbb49145f81d08011
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-14 20:07:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ document.getElementById("title").style.display = 'block';
 function gameStart(){
   document.getElementById("title").style.display = 'none';
 }
+gamespace = document.getElementById("gameSpace")
 console.log(document.getElementById("gameSpace"));
+gamespace.innerText = "<p message>a</p>";
