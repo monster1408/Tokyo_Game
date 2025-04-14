db1f0e72143e54d602fa958588275aaaee35c1eb
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-14 20:38:40 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+gamespace = document.getElementById("gameSpace")
+console.log(document.getElementById("gameSpace"));
+document.getElementById("title").style.display = 'block';
+function gameStart(){
+  document.getElementById("title").style.display = 'none';
+  gamespace.innerHTML = "<p message>a</p>";
+}
