ca0bd9e2d87f30d098fa4e9cc6cbec6d4493dee6
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-16 22:15:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-gamespace = document.getElementById("gameSpace")
+messagenum = 0;
+gamespace = document.getElementById("gameSpace");
+message = document.getElementById("message");
 console.log(document.getElementById("gameSpace"));
 document.getElementById("title").style.display = 'block';
+messagelist = ["あなたはどこにしますか?","千代田区?品川区?"]
 function gameStart(){
   document.getElementById("title").style.display = 'none';
- gamespace.innerHTML = '<p id="message">こんにちは</p>';
+  gamespace.innerHTML = '<p id="message">こんにちは</p>';
+}
+function messagefunction() {
+  message.innerText=messagelist[messagenum]
 }
