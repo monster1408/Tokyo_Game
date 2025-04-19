f6953d2b6c533710cc7623d8b65f9d7778d918e5
monster1408 <yukiya.toyohara@hotmail.com>
2025-04-19 13:03:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ gamespace = document.getElementById("gameSpace");
 message = document.getElementById("message");
 console.log(document.getElementById("gameSpace"));
 document.getElementById("title").style.display = 'block';
-messagelist = ["こんにちは。","こちらはエリアの選択画面となっております。",
+messagelist = ["こちらはエリアの選択画面となっております。",
 "あなたが選んだ区は今後の体験に多大な影響を及ぼす可能性を有しています。",
 "焦らず、しかし迅速にお決めいただくことを推奨いたします。"]
 function gameStart(){
   document.getElementById("title").style.display = 'none';
-  gamespace.innerHTML = '<p id="message">こんにちは</p><a id=nextmessage onclick="messagefunction()">Next</a>';
+  gamespace.innerHTML = '<p id="message">こんにちは。</p><a id=nextmessage onclick="messagefunction()">Next</a>';
 }
 function messagefunction() {
   const message = document.getElementById("message"); // 毎回取得
