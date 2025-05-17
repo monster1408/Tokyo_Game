96bc646d8ec36ba15abd2de76a861797aee05b47
monster1408 <yukiya.toyohara@hotmail.com>
2025-05-17 15:31:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function gameStart(){
 function messagefunction() {
   messagelist = messagelist = ["こちらはエリアの選択画面となっております。",
 "あなたが選んだ区は今後の体験に多大な影響を及ぼす可能性を有しています。",
-"焦らず、しかし迅速にお決めいただくことを推奨いたします。","決めたら次へを押してください","いい選択です。"+PlayerSelect+"はいい場所ですよ!","人口は"+tokyoWards[i].population+"人です"]
+"焦らず、しかし迅速にお決めいただくことを推奨いたします。","決めたら次へを押してください","いい選択です。"+PlayerSelect+"はいい場所ですよ!","人口は"+tokyoWards[i].population+"人です"];
   const message = document.getElementById("message");
   if (messagenum < messagelist.length) {
     message.innerText = messagelist[messagenum];
